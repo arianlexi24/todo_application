d9ea5c862e3942477c75f236845e4090739b8ad2
Christian <rakoto.christall@gmail.com>
2018-03-09 18:18:30 +0300

    Premier commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "todo",
+    'description': "Gestion de tache a faire",
+
+    'author': "Christian Rakotomalala",
+    'depends': ['base'],
+    'data': ['views/todo_views.xml', 'views/todo_menu.xml'],
+    'application': True,
+}
