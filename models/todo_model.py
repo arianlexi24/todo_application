066db856134b433cc49d16363c7dddd86097efb0
Christian <rakoto.christall@gmail.com>
2018-03-09 20:20:42 +0300

    Changement nom de la vue créée

--- models/todo_model.py
+++ models/todo_model.py
@@ -7,5 +7,5 @@ class TodoTask(models.Model):
     _name = 'todo.task'
     _description = 'To-do Task'
     name = fields.Char('Description', required=True)
-    is_done = fields.Boolean('Done?')
-    active = fields.Boolean('Active?', default=True)
+    is_done = fields.Boolean('Tâche terminée?')
+    active = fields.Boolean('Tâche en cours?', default=True)
